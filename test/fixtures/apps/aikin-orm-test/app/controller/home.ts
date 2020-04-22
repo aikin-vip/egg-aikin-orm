4b6015cc2b88384f0ddcd020db41da4297ae7ede
aikin <aikin.vip.1022@gmail.com>
2020-04-22 16:57:30 +0800

    publish

--- test/fixtures/apps/aikin-orm-test/app/controller/home.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from 'egg';
-
-class HomeController extends Controller {
-  async index() {
-    this.ctx.body = 'hi, ' + this.app.plugins.orm.name;
-  }
-}
-
-module.exports = HomeController;
--- test/fixtures/apps/aikin-orm-test/app/router.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-module.exports = app => {
-    const { router, controller } = app;
-    router.get('/', controller.home.index);
-};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicm91dGVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsicm91dGVyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFFQSxNQUFNLENBQUMsT0FBTyxHQUFHLEdBQUcsQ0FBQyxFQUFFO0lBQ3JCLE1BQU0sRUFBRSxNQUFNLEVBQUUsVUFBVSxFQUFFLEdBQUcsR0FBRyxDQUFDO0lBRW5DLE1BQU0sQ0FBQyxHQUFHLENBQUMsR0FBRyxFQUFFLFVBQVUsQ0FBQyxJQUFJLENBQUMsS0FBSyxDQUFDLENBQUM7QUFDekMsQ0FBQyxDQUFDIn0=
